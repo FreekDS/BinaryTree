8f39d0a5bd0d93d853e7744646fd2e3f01faaddf
FreekDS <36729091+FreekDS@users.noreply.github.com>
2020-04-26 14:11:19 +0000

    Deploying to gh-pages from  @ 10127fb410a231e2c7484984d0c4572fa092f6f8 🚀

---
+++ precache-manifest.261a0e6502d9b781dcb0f8d4db971e3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51fcd0cc268467baee2ced4c4caa72b",
+    "url": "/BinaryTree/index.html"
+  },
+  {
+    "revision": "e6a88f830e0b23ee3668",
+    "url": "/BinaryTree/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "548bac9ed9171fabf5d7",
+    "url": "/BinaryTree/static/css/main.059f545e.chunk.css"
+  },
+  {
+    "revision": "e6a88f830e0b23ee3668",
+    "url": "/BinaryTree/static/js/2.65f2bbf0.chunk.js"
+  },
+  {
+    "revision": "4b3a5ba57b1f18ea8e3bc9f3de40315e",
+    "url": "/BinaryTree/static/js/2.65f2bbf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548bac9ed9171fabf5d7",
+    "url": "/BinaryTree/static/js/main.f4252ebf.chunk.js"
+  },
+  {
+    "revision": "86211ab7cdf0d9c41800",
+    "url": "/BinaryTree/static/js/runtime-main.dc25435b.js"
+  },
+  {
+    "revision": "024572bbf78191dafd22afd61ed4ada0",
+    "url": "/BinaryTree/static/media/logo-UA.024572bb.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BinaryTree/precache-manifest.163dfebdcf6c145de1bb97ca91104f46.js"
+  "/BinaryTree/precache-manifest.261a0e6502d9b781dcb0f8d4db971e3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BinaryTree/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
